7785b40f8d2818a091f60cd3ae7d1291ef8fc09f
gm_desktop <gmesda23@gmail.com>
2020-01-14 18:47:52 -0500

    added new leetcode questions

---
+++ medium_of_the_two_arrays.js
@@ -0,0 +1,12 @@
+var findMedianSortedArrays = function(nums1) {
+    // % remainer operator 
+    // The JavaScript Math object allows you 
+    //to perform mathematical tasks on numbers.
+    // Return the number of characters in a string:
+    // return console.log(Math.floor(nums1.length))
+    for (const i in nums1) {
+    }
+    
+};
+
+findMedianSortedArrays([7,2,5])

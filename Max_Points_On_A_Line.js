d6dd1d98bb3d708e639f89d06877620732e8893e
gm_desktop <gmesda23@gmail.com>
2020-01-31 22:08:48 -0500

    almost done finding the true answer to Max_Points in a line

--- Max_Points_On_A_Line.js
+++ Max_Points_On_A_Line.js
@@ -44,26 +44,41 @@ Explanation:
  */
 
 var maxPoints = function(points) {
-    // for( var j = 0; j < points.length; j++) {
-    //     console.log(points[j])
-    // }
-    return points.reduce(function(accumulator, currentValue, currentIndex, array) {
-        var map = {POSITIVE_INFINITY: 0}
-        var samePoint = 1;
 
-        for( var j = 0; j < points.length; j++) {
-            console.log(points[j])
+    return points.reduce(function(accumulator, currentValue, currentIndex, array) {
+        var chart = {POSITIVE_INFINITY: 0} // This is the chart, we inlcuce 0 beause x:0, y:0
+        var samePoint = 1; // This is the initial point, x:1, y:1 
+
+
+        for( var j = currentIndex + 1; j < points.length; j++) {
+            var p2 = points[j];
+            
+            // array[0] becomes array x which is point x, same for array[1] and y
+            if ( p2[0] === currentValue[0] && p2[1] === currentValue[1] ){
+                console.log(samePoint++, 'samePoint ++')
+            } else {
+                var slope =  p2[0] === currentValue[0] ? Number.POSITIVE_INFINITY :  10000000*(p2[0] - currentValue[0]) / (p2[1] - currentValue[1])  
+                console.log(slope,  'slope ')
+                chart[slope] =  ( chart[slope] || 0 ) + 1;
+
+                console.log(chart[slope], 'chart[slope]')
+            }
+            // console.log(points[j])
         }
+        return console.log(   Math.max(accumulator, ...Object.keys(chart).map(k => chart[k] + samePoint))  );
     },0)
 };
 
-var maxPoints = function(points) {
-    var calculateSlope = function(x1, y1, x2, y2) {
-        var slope =  (y1 - y2) / (x2 - x1)
-    }
+// var maxPoints = function(points) {
+//     var calculateSlope = function(x1, y1, x2, y2) {
+//         var slope =  (y1 - y2) / (x2 - x1)
+//     }
 
-    return slope;
-}
+//     return slope;
+// }
 
-maxPoints([[1,1],[2,2],[3,3]]);
+// STILL NEED TO REVIEW 
+// one of the responses returns NaN
+
+maxPoints([[0,0], [0,0]]);
 // maxPoints([1,2,3]);

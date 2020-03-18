e3265621282d94d5aef5d6c70a8e9e40d6491fab
gm_desktop <gmesda23@gmail.com>
2020-03-18 17:08:59 -0400

    updated merge sort

--- SortingAlgos/Javascript/MergeSort.js
+++ SortingAlgos/Javascript/MergeSort.js
@@ -1,4 +1,7 @@
+/*
+Merge Sort is a sorting algorithm, which is commonly used in computer science. Merge Sort is a divide and conquer algorithm. It works by recursively breaking down a problem into two or more sub-problems of the same or related type, until these become simple enough to be solved directly. The solutions to the sub-problems are then combined to give a solution to the original problem. So Merge Sort first divides the array into equal halves and then combines them in a sorted manner.
 
+*/
 
 function mergeSort(arr) {
     // Check if the array can be split

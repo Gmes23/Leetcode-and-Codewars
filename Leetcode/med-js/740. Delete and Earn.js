7b6d85a1a32deb4553690f9f86aef60c3d90bc7d
gm_desktop <gmesda23@gmail.com>
2020-03-30 23:52:20 -0400

    updated leetcode

---
+++ Leetcode/med-js/740. Delete and Earn.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var deleteAndEarn = function(nums) {
+    
+  
+
+    maxpoints = nums.reduce((a,b) => a + b , 0 )
+    var max = Math.max(...nums);
+    return max;
+};
+
+console.log(deleteAndEarn([2,3,4]))

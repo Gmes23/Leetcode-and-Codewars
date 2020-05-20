b61912b5166c4e5189e5aad1428d25975946a45c
gm_desktop <gmesda23@gmail.com>
2020-05-19 21:27:09 -0400

    added new leet code

---
+++ Leetcode/easy-js/1189. Maximum Number of Balloons.js
@@ -0,0 +1,56 @@
+1189. Maximum Number of Balloons
+Easy
+
+221
+
+31
+
+Add to List
+
+Share
+Given a string text, you want to use the characters of text to form as many instances of the word "balloon" as possible.
+
+You can use each character in text at most once. Return the maximum number of instances that can be formed.
+
+ 
+
+Example 1:
+
+
+
+Input: text = "nlaebolko"
+Output: 1
+Example 2:
+
+
+
+Input: text = "loonbalxballpoon"
+Output: 2
+Example 3:
+
+Input: text = "leetcode"
+Output: 0
+ 
+
+Constraints:
+
+1 <= text.length <= 10^4
+text consists of lower case English letters only.
+
+
+
+/**
+ * @param {string} text
+ * @return {number}
+ */
+var maxNumberOfBalloons = (S) => {
+    return Math.floor(Math.min(
+        [...S].filter(c => c === 'b').length,
+        [...S].filter(c => c === 'a').length,
+        [...S].filter(c => c === 'l').length / 2,
+        [...S].filter(c => c === 'o').length / 2,
+        [...S].filter(c => c === 'n').length,
+    )) || 0;
+  };
+
+console.log(maxNumberOfBalloons('loonbalxballpoon'))

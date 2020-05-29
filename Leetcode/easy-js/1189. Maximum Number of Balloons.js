27d6835806915f0134046167fe99872dd0373401
gm_desktop <gmesda23@gmail.com>
2020-05-28 22:38:32 -0400

    removed whitespace

--- Leetcode/easy-js/1189. Maximum Number of Balloons.js
+++ Leetcode/easy-js/1189. Maximum Number of Balloons.js
@@ -6,11 +6,7 @@ Given a string text, you want to use the characters of text to form as many inst
 
 You can use each character in text at most once. Return the maximum number of instances that can be formed.
 
- 
-
 Example 1:
-
-
 
 Input: text = "nlaebolko"
 Output: 1
